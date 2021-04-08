4106992a222d0d27a0cec75941969a860ca92ac4
maettuki <dbguswn001@naver.com>
2021-04-08 16:59:49 +0900

    Update test.py

--- gsm/test.py
+++ gsm/test.py
@@ -1,22 +1,26 @@
-from  konlpy.tag import *
-import matplotlib.pyplot as plt
-from matplotlib import  font_manager , rc
-from wordcloud import WordCloud
-from collections import Counter
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import pandas as pd
 
-okt=Okt()
+query_txt="빅데이터"
+f_name="D:/3학년/개발/빅데이터/TREE/Data/빅데이터_d.txt"
+fc_name="D:/3학년/개발/빅데이터/TREE/Data/빅데이터_d.csv"
+path="C:/Temp/chromedriver.exe"
+driver= webdriver.Chrome(path)
 
-data1=open("D:/3학년/개발/빅데이터/TREE/Data/빅데이터.txt").read()
-data2=okt.nouns(data1)
+driver.get("https://www.daum.net/")
+time.sleep(2)
+driver.maximize_window()
 
-data3=[]
-for a in data2:
-    if a == "제목":
-        data3.append(a.replace("제목", "유현주"))
-    elif a == "내용":
-        data3.append(a.replace("내용", "유현주"))
-    elif a == "번호":
-        data3.append(a.replace("번호", ""))
-    else:
-        data3.append(a)
-print(data3)
+element= driver.find_element_by_id("q")
+element.send_keys(query_txt)
+element.submit()
+
+driver.find_element_by_link_text("뉴스").click()
+
+number=[]
+title_l=[]
+content_l=[]
+
+no=1
