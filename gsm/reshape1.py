67647af7b234e387cc053d9cbe6ef8e81df01675
maettuki <dbguswn001@naver.com>
2021-04-22 15:45:29 +0900

    Create reshape1.py

---
+++ gsm/reshape1.py
@@ -0,0 +1,46 @@
+import numpy as np
+import random
+
+x=np.arange(12)
+print(x)
+y=x.reshape(3,4)
+print(y)
+z=y.flatten()
+print(z)
+
+
+
+print(np.random.rand(5))
+print(np.random.rand(5,3))
+print(np.random.randint(1,10,size=10))
+print(np.random.randint(1,100,size=(3,5)))
+
+
+print("end")
+data_2=np.random.randint(1,10,size=10)
+print(data_2)
+print(data_2[::-1])
+print(np.mean(data_2))
+print(np.median(data_2))
+
+print(data_2.sum())
+print(data_2.mean())
+
+print("end")
+x=[i for i in range(100)]
+y=[i ** 4 for i in range(100)]
+print(x)
+print(y)
+result=np.corrcoef(x,y)
+print(result)
+
+print("end")
+
+x=[i for i in range(10)]
+y=[i ** 2 for i in range(10)]
+z=[i for i in range(10,0,-1)]
+print(x)
+print(y)
+print(z)
+result= np.corrcoef([x,y,z])
+print(result)
