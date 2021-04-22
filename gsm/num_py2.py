4adcfb10eb90eb4c57511c7c9feff24ff0f04563
maettuki <dbguswn001@naver.com>
2021-04-22 14:50:56 +0900

    numpy

---
+++ gsm/num_py2.py
@@ -0,0 +1,6 @@
+import numpy as np
+mid_scores=np.array([10,20,30])
+final_scores=np.array([60,70,80])
+total=mid_scores+final_scores
+print("시험성적의 합계 :",total)
+print("시험성적의 평균 :",total/2)
--- gsm/text_num.py
+++ gsm/text_num.py
@@ -42,3 +42,7 @@ output=re.findall('\S+@[a-z.]+',txt)
 for text in output:
     text_split=text.split('@')
     print("추출된 아이디"+text_split[0]+"도메인:"+text_split[1])
+
+s="My lucky number 27 99"
+print(re.sub("[0-9]+","*",s))
+print(re.sub('\d+','*',s))
